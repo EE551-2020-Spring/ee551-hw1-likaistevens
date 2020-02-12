e746a649413092fb062b5d334bd5ee0d79426d8a
likaistevens <55108327+likaistevens@users.noreply.github.com>
2020-02-12 15:37:49 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -41,25 +41,26 @@ def lists():
     p = n.split()
 
     # Get all the items past the first of the third substring
-    
+    r = p[1:]
 
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-    c = [[1, 4, 5],[6, 10, 11],[12, 17, 38]]
+    A = [[1, 4, 5],[6, 10, 11],[12, 17, 38]]
 
     # Collect the items in the last column of matrix A using list comprehension
-    d = [x ]
+    c = [i[-1] for i in A]
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
-
+    d = [A[i][i] for i in range(0,3) if A[i][i]%2==0]
+    
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
+    o = [ord(i) for i in "Stevens"]
 
     return p, r, c, d, o
-
 
 def dictionaries():
     """
@@ -69,38 +70,37 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-    f = {fruit:"apple",quantity:4,color:green"}
+    a={"fruit":"apple","quantity":4,"color":"green"}
 
     # Get the item in dictionary f that the key "fruit" maps to
-    a = f[fruit] 
+    f = a["fruit"] 
 
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
-    f[quantity]+1
+    a["quantity"]+=1
 
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-    p = {name:{first_name => "Grace", last_name => "Hopper"},jobs:["scientist", "engineer"],age:85}
+    amazing_grace = {"name":{"first_name":"Grace", "last_name":"Hopper"},"jobs":["scientist", "engineer"],"age":85}
 
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-    p[jobs].append("programmer")
+    amazing_grace["jobs"].append("programmer")
 
     # Get the third job Grace has that you recently added
-    k = p[jobs][2]
+    p = amazing_grace["jobs"][2]
 
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
-    
+    k = sorted(amazing_grace.keys())
 
     return a, f, p, k
+
 
 numbers_and_strings()
 lists()
 dictionaries()
-
-
 
 
 
